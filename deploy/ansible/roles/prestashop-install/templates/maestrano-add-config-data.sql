92cc1a1f965d744c58fe2c088b8f9e30745de981
BrunoChauvet <bruno.chauvet@maestrano.com>
2015-11-05 22:06:05 +1100

    revert SQL config update

--- deploy/ansible/roles/prestashop-install/templates/maestrano-add-config-data.sql
+++
@@ -1,3 +0,0 @@
-UPDATE ps_configuration SET value = '1' WHERE name = 'PS_REWRITING_SETTINGS';
-UPDATE ps_configuration SET value = '{{ server_hostname }}' WHERE name = 'PS_SHOP_DOMAIN';
-UPDATE ps_configuration SET value = '{{ server_hostname }}' WHERE name = 'PS_SHOP_DOMAIN_SSL';
