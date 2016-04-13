d306b4bc46f901e9ba5d98a112aa0bbee538ce5a
bruno <bruno.chauvet@maestrano.com>
2016-04-13 15:12:14 +1000

    improve configuration time

--- deploy/ansible/roles/prestashop-install/templates/maestrano-add-config-data.sql
+++ deploy/ansible/roles/prestashop-install/templates/maestrano-add-config-data.sql
@@ -3,3 +3,4 @@ UPDATE ps_configuration SET value = '{{ server_hostname }}' WHERE name = 'PS_SHO
 UPDATE ps_configuration SET value = '{{ server_hostname }}' WHERE name = 'PS_SHOP_DOMAIN_SSL';
 UPDATE ps_configuration SET value = {% if maestrano_environment == 'local' %}'0'{% else %}'1'{% endif %} WHERE name = 'PS_SSL_ENABLED';
 INSERT INTO ps_configuration (name, value, date_add, date_upd) VALUES('PS_SSL_ENABLED_EVERYWHERE', '1', '0000-00-00 00:00:00', '0000-00-00 00:00:00') ON DUPLICATE KEY UPDATE value=VALUES(value);
+UPDATE ps_shop_url SET domain = '{{ server_hostname }}', domain_ssl = '{{ server_hostname }}';
